1cb1e1b05f7bda44d9b58aafdaa6c97c9a53e047
Leticia-del <61697988+Leticia-del@users.noreply.github.com>
2020-06-06 15:01:18 -0300

    Update calculozinho.js

--- www/components/calculozinho.js
+++ www/components/calculozinho.js
@@ -1,11 +1,12 @@
 $(document).on('click', '#calculo', function() {
  var a = parseInt($('#idade').val()); 
+ var b = parseInt($('#nome').val()); 
 
 if (a >= 18) {
-  $('#aviso').val("Você é +18 anos");
+  $('#aviso').val(b "é +18 anos");
   $('#situação').html("<img src='lib/velho.png' heigth='300vh' width='300vh'>");
 }else {
-$('#aviso').val("Você é -18 anos");
+$('#aviso').val(b "tem -18 anos");
 $('#situação').html("<img src='lib/crianca.png' heigth='300vh' width='300vh'>");
 }
 });
